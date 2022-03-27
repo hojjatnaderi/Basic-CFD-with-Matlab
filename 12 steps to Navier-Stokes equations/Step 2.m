b243d5a31bef58fb9e9388892f632e63f0415cbb
hojnaderi <101932132+hojnaderi@users.noreply.github.com>
2022-03-27 13:44:07 +0430

    Update Step 2.m

--- 12 steps to Navier-Stokes equations/Step 2.m
+++ 12 steps to Navier-Stokes equations/Step 2.m
@@ -1,25 +1,27 @@
 clc
-clear all
 close all
+clear
+%Setting up the field
 nx=41;
 nt=20;
 dx=(2-0)/(nx-1);
 dt=0.025;
-%defining u_zero
+%Setting up u_zero
 u=ones(1,nx);
 u(int64((0.5-0)/dx): int64((1-0)/dx+1))=2;
-
+%Plotting u_zero
 u_zero=u;
 x=(0:dx:2);
 %plot(x,u_zero)
 %hold on
-%defining un middle parameter
+%defining un parameter
 un=ones(1,nx);
+%Iterating
  for n=1:nt
      un=u;
      for i=2:nx
          u(i)=un(i)-(un(i))*(dt/dx)*(un(i)-un(i-1));
      end
  end
- 
+ %Plotting output
  plot(x,u)
