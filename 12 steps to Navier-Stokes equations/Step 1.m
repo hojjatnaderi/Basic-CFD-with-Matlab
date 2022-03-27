980309186281d91a48fd1e5a55ca77f016eba1e9
hojnaderi <101932132+hojnaderi@users.noreply.github.com>
2022-03-27 13:31:04 +0430

    Update Step 1.m

--- 12 steps to Navier-Stokes equations/Step 1.m
+++ 12 steps to Navier-Stokes equations/Step 1.m
@@ -1,20 +1,24 @@
 clc;
+close all;
 clear;
+%Setting up the field
 nx=41;
 dx=2/(nx-1);
 nt=25;
 dt=0.025;
 c=1;
 x=(0:dx:2);
+%Setting up the the initial condition
 u=ones(1,nx);%u_zero
+%below loop is same as: u(int64(.5/dx):int64(1/dx+1))=2;
 for i=1:nx
     if (0.5<=x(i)) && (x(i)<=1)
         u(i)=2;
     end
 end
-
-un=ones(1,nx);%un is middle parameter and is "u in time"
-
+%Setting up go-between parameter un
+un=ones(1,nx);
+%Iterating
 for n=1:nt
     un=u;%setting u_zero to un
     for i=2:nx
