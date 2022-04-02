0cdcd9ed16422df33ae6d92a5cd83bdeefd535e3
Hojjat Naderi <101932132+hojnaderi@users.noreply.github.com>
2022-04-02 19:03:20 +0430

    Update Step 11.m

--- 12 steps to Navier-Stokes equations/Step 11.m
+++ 12 steps to Navier-Stokes equations/Step 11.m
@@ -1,5 +1,4 @@
 %Navier Stokes - Cavity flow
-%For discussion on how to improve this code refer to Step 06
 clc
 close all
 clear
@@ -31,53 +30,51 @@ pn=zeros(ny,nx);
 
 %Iteration
 for n=1:nt
+    
     un=u;
     vn=v;
     pn=p;
-    for j=2:ny-1
-        for i=2:nx-1
-            
-            u(j,i)=un(j,i)-un(j,i)*(dt/dx)*(un(j,i)-un(j,i-1))...
-                -vn(j,i)*(dt/dy)*(un(j,i)-un(j-1,i))...
-                -(dt/(rho*2*dx))*(pn(j,i+1)-pn(j,i-1))...
-                +nu*((dt/dx^2)*(un(j,i+1)-2*un(j,i)+un(j,i-1))...
-                +(dt/dy^2)*(un(j+1,i)-2*un(j,i)+un(j-1,i)));
-            
-            v(j,i)=vn(j,i)-un(j,i)*(dt/dx)*(vn(j,i)-vn(j,i-1))...
-                -vn(j,i)*(dt/dy)*(vn(j,i)-vn(j-1,i))...
-                -(dt/(rho*2*dy))*(pn(j+1,i)-pn(j-1,i))...
-                +nu*((dt/dx^2)*(vn(j,i+1)-2*vn(j,i)+vn(j,i-1))...
-                +(dt/dy^2)*(vn(j+1,i)-2*vn(j,i)+vn(j-1,i)));
-            
-            b(j,i)=(1/(2*dt))*(((un(j,i+1)-un(j,i-1))/dx)+((vn(j+1,i)-vn(j-1,i))/dy))...
-                -((un(j,i+1)-un(j,i-1))/(2*dx))*((un(j,i+1)-un(j,i-1))/(2*dx))...
-                -2*((un(j+1,i)-un(j-1,i))/(2*dy))*((vn(j,i+1)-vn(j,i-1))/(2*dx))...
-                -((vn(j+1,i)-vn(j-1,i))/(2*dy))*((vn(j+1,i)-vn(j-1,i))/(2*dy));
-                                              
-            p(j,i)=((pn(j,i+1)+pn(j,i-1))*(dy^2)+(pn(j+1,i)+pn(j-1,i))*(dx^2))...
-                /(2*(dx^2+dy^2))-(rho*(dx^2)*(dy^2)*b(j,i))/(2*(dx^2+dy^2));
-
-            %Boundary conditions           
-            u(end,:)=1; %lid movement
-            u(1,:)=0;
-            v(end,:)=0;
-            v(1,:)=0;
-            
-            u(:,1)=0;
-            u(:,end)=0;
-            v(:,1)=0;
-            v(:,end)=0;
-            
-            p(end,:)=0;
-            p(1,:)=p(2,:); %dp/dy
-            
-            p(:,1)=p(:,2); %dp/dx
-            p(:,end)=p(:,end-1); %dp/dx
+    
+    u(2:end-1,2:end-1)=un(2:end-1,2:end-1)...
+        -un(2:end-1,2:end-1)*(dt/dx).*(un(2:end-1,2:end-1)-un(2:end-1,1:end-2))... % .* is important but here it shows no effect than *
+        -vn(2:end-1,2:end-1)*(dt/dy).*(un(2:end-1,2:end-1)-un(1:end-2,2:end-1))...
+        -(dt/(rho*2*dx))*(pn(2:end-1,3:end)-pn(2:end-1,1:end-2))...
+        +nu*((dt/dx^2)*(un(2:end-1,3:end)-2*un(2:end-1,2:end-1)+un(2:end-1,1:end-2))...
+        +(dt/dy^2)*(un(3:end,2:end-1)-2*un(2:end-1,2:end-1)+un(1:end-2,2:end-1)));
+    
+    v(2:end-1,2:end-1)=vn(2:end-1,2:end-1)...
+        -un(2:end-1,2:end-1)*(dt/dx).*(vn(2:end-1,2:end-1)-vn(2:end-1,1:end-2))...
+        -vn(2:end-1,2:end-1)*(dt/dy).*(vn(2:end-1,2:end-1)-vn(1:end-2,2:end-1))...
+        -(dt/(rho*2*dy))*(pn(3:end,2:end-1)-pn(1:end-2,2:end-1))...
+        +nu*((dt/dx^2)*(vn(2:end-1,3:end)-2*vn(2:end-1,2:end-1)+vn(2:end-1,1:end-2))...
+        +(dt/dy^2)*(vn(3:end,2:end-1)-2*vn(2:end-1,2:end-1)+vn(1:end-2,2:end-1)));
+    
+    b(2:end-1,2:end-1)=(1/(2*dt))*(((un(2:end-1,3:end)-un(2:end-1,1:end-2))/dx)+((vn(3:end,2:end-1)-vn(1:end-2,2:end-1))/dy))...
+        -((un(2:end-1,3:end)-un(2:end-1,1:end-2))/(2*dx)).*((un(2:end-1,3:end)-un(2:end-1,1:end-2))/(2*dx))...
+        -2*((un(3:end,2:end-1)-un(1:end-2,2:end-1))/(2*dy)).*((vn(2:end-1,3:end)-vn(2:end-1,1:end-2))/(2*dx))...
+        -((vn(3:end,2:end-1)-vn(1:end-2,2:end-1))/(2*dy)).*((vn(3:end,2:end-1)-vn(1:end-2,2:end-1))/(2*dy));
+    
+    p(2:end-1,2:end-1)=((pn(2:end-1,3:end)+pn(2:end-1,1:end-2))*(dy^2)+(pn(3:end,2:end-1)+pn(1:end-2,2:end-1))*(dx^2))...
+        /(2*(dx^2+dy^2))-(rho*(dx^2)*(dy^2)*b(2:end-1,2:end-1))/(2*(dx^2+dy^2));
+    
+    %Boundary conditions
+    u(end,:)=1; %lid movement
+    u(1,:)=0;
+    v(end,:)=0;
+    v(1,:)=0;
+    
+    u(:,1)=0;
+    u(:,end)=0;
+    v(:,1)=0;
+    v(:,end)=0;
+    
+    p(end,:)=0;
+    p(1,:)=p(2,:); %dp/dy
+    
+    p(:,1)=p(:,2); %dp/dx
+    p(:,end)=p(:,end-1); %dp/dx
                                   
-        end
-    end
 end
-
 %Output
 contourf(x,y,p);
 hold on
